d1a16fc649124240436d0c5d4ef08b3470cf927c
BrettWolfinger <BrettWolfinger@users.noreply.github.com>
2026-05-28 17:23:19 +0000

    deploy: a1110e5c9d6c3c9eb22f8fd65603e869986e3867

--- api/search/all_2.js
+++ api/search/all_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['emit_0',['emit',['../classEngine_1_1EventDispatcher.html#a641c03f1d2f4ab043cee1e3bfbc2eee1',1,'Engine::EventDispatcher::emit()'],['../Events_8h.html#a4820028c2adf7967a1a1b924b03bece9',1,'Engine::Events::emit()'],['../Particles_8h.html#ac2d8ab52285b810fe1b0b5a97a047b8c',1,'Engine::Particles::emit()']]],
+  ['emit_0',['emit',['../Events_8h.html#a4820028c2adf7967a1a1b924b03bece9',1,'Engine::Events::emit()'],['../Particles_8h.html#ac2d8ab52285b810fe1b0b5a97a047b8c',1,'Engine::Particles::emit()']]],
   ['emiterased_1',['emitErased',['../classEngine_1_1EventDispatcher.html#a44ce240420bda3dcffd0f74840505476',1,'Engine::EventDispatcher']]],
   ['endsize_2',['endSize',['../structEngine_1_1ParticleEmitParams.html#a6700f0c3ae3582fa08208bd43e03f66d',1,'Engine::ParticleEmitParams']]],
   ['engine_2eh_3',['Engine.h',['../Engine_8h.html',1,'']]],
--- api/search/functions_a.js
+++ api/search/functions_a.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['stoploopingvoice_0',['stopLoopingVoice',['../Audio_8h.html#a52c1639e140f089a2f99f3d221ba93ea',1,'Engine::Audio']]],
-  ['subscribe_1',['subscribe',['../classEngine_1_1EventDispatcher.html#aeec104326e1990c0b6509925ea50e9d3',1,'Engine::EventDispatcher']]],
-  ['subscribeerased_2',['subscribeErased',['../classEngine_1_1EventDispatcher.html#a2af750f6090e5d59877ca5c7e1a1c31c',1,'Engine::EventDispatcher']]]
+  ['subscribeerased_1',['subscribeErased',['../classEngine_1_1EventDispatcher.html#a2af750f6090e5d59877ca5c7e1a1c31c',1,'Engine::EventDispatcher']]]
 ];
